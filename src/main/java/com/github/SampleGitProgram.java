9189696e6c72e42aa5a627f00e52afa262d546df
FARHAN <FARHAN@DESKTOP-1DLRFCA>
2022-08-24 11:55:41 +0530

    gopuffScript

---
+++ src/main/java/com/github/SampleGitProgram.java
@@ -0,0 +1,16 @@
+package com.github;
+
+public class SampleGitProgram {
+	public static void main(String[] args) {
+		System.out.println("project created");
+		System.out.println("Welcome to our goPuff Project");
+		
+	}
+	
+	
+
+		}
+
+	
+
+
